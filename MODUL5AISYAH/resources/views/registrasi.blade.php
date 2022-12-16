267a5d0661da15f242f0a9ab776c71ae672f2ef4
aisyahaydila <aisyahaydila@gmail.com>
2022-12-16 08:57:14 +0700

    finnal

--- MODUL5AISYAH/resources/views/registrasi.blade.php
+++ MODUL5AISYAH/resources/views/registrasi.blade.php
@@ -1,11 +1,12 @@
-
+@extends('layout')
+@section('content')
 <!--FORM-->
 <body>
     <div class="d-flex justify-content-between align-items-center" style="height: 100vh;">
     <div class="img" style="margin-left:0px;">
         <img src="./gambar/raize.jpg" style="transform: scaleX(-1);width: 700px; height: 713px;" alt="">
     </div>
-    <form action="{{route('register.action')}" method="post" style="width:50%;margin: 10px 50px">
+    <form action="{route('register.action')}" method="post" style="width:50%;margin: 10px 50px">
     @csrf
     <h1 style="padding-bottom:20px;">Registrasi</h1>
     <div class="col-md-9">
@@ -38,3 +39,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
+@endsection
--- MODUL5AISYAH/routes/web.php
+++ MODUL5AISYAH/routes/web.php
@@ -14,14 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('edititem', ['title' => 'Home']);
-})->name('home');
+Route::get('/registrasi', function () {
+    return view('registrasi');
+});
 
-Route::get('register', [UserController::class, 'register'])->name('register');
-Route::post('register', [UserController::class, 'register_action'])->name('register.action');
-Route::get('login', [UserController::class, 'login'])->name('login');
-Route::post('login', [UserController::class, 'login_action'])->name('login.action');
-Route::get('password', [UserController::class, 'password'])->name('password');
-Route::post('password', [UserController::class, 'password_action'])->name('password.action');
-Route::get('logout', [UserController::class, 'logout'])->name('logout');
